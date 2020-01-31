62f362fbd382d66aa37aab459f0467aeffb31fd6
pieris98 <pieriskalligeros@yahoo.gr>
2020-01-31 17:39:56 +0000

    Working Protected Confirmation Test

--- app/src/main/java/pkeros/com/user/teedummy/MainActivity.kt
+++ app/src/main/java/pkeros/com/user/teedummy/MainActivity.kt
@@ -1,5 +1,6 @@
 package pkeros.com.user.teedummy
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.security.ConfirmationCallback
@@ -7,10 +8,15 @@ import android.security.ConfirmationPrompt
 import android.util.Log
 import android.security.keystore.KeyProperties
 import android.security.keystore.KeyGenParameterSpec
+import android.support.v7.app.AlertDialog
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.security.*
 import java.util.concurrent.Executor
+import android.content.DialogInterface
+import android.util.Base64
+import kotlin.coroutines.experimental.coroutineContext
+
 
 
 // This data structure varies by app type. This is just an example.
@@ -25,16 +31,26 @@ class MyConfirmationCallback : ConfirmationCallback() {
         // By completing this process, you generate a "signed statement".
         var keyStore= KeyStore.getInstance("AndroidKeystore")
         keyStore.load(null)
-        val secretKeyEntry = keyStore
-                .getEntry("key1", null) as KeyStore.SecretKeyEntry
-
         val privateKeyEntry = keyStore
-                .getEntry("key1", null) as KeyStore.PrivateKeyEntry
+                ?.getEntry("key1", null) as KeyStore.PrivateKeyEntry
 
         val privateKey = privateKeyEntry.privateKey
-        val signature = Signature.getInstance("SHA256withRSA/PSS")
-        signature.initSign(privateKey)
-        signature.sign(dataThatWasConfirmed,0,128)
+
+        try {
+            val signature = Signature.getInstance("SHA256withRSA/PSS")
+            signature.initSign(privateKey)
+            signature!!.update(dataThatWasConfirmed)
+            val signatureBytes = signature!!.sign()
+
+            // dataThatWasConfirmed and signatureBytes should be sent to RP
+            // RP verifies the signature with Kpub and the message (dataThatWasConfirmed) is identical to the challenge
+            Log.i("[MainActivity]", "dataThatWasConfirmed: " + Base64.encodeToString(dataThatWasConfirmed, Base64.URL_SAFE))
+            Log.i("[MainActivity]", "signature: " + Base64.encodeToString(signatureBytes, Base64.URL_SAFE))
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+
+
     }
 
     override fun onDismissed() {
@@ -49,43 +65,54 @@ class MyConfirmationCallback : ConfirmationCallback() {
         // could respond to the prompt.
     }
 
-   /*override fun onError(e: Exception?) {
+   fun onError(e: Exception?) {
         super.onError(e)
         Log.println(Log.INFO,"onError",e.toString())
 
         // Handle the exception that the callback captured.
-    }*/
+    }
 }
 
 class MainActivity : AppCompatActivity() {
-
+    var keyStore= KeyStore.getInstance("AndroidKeystore")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bodyText.text=ConfirmationPrompt.isSupported(this).toString()
         //generating private key
-/*        val keyPairGenerator = KeyPairGenerator.getInstance(
+        val keyPairGenerator = KeyPairGenerator.getInstance(
                 KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore")
         keyPairGenerator.initialize(
                 KeyGenParameterSpec.Builder(
                         "key1",
                         KeyProperties.PURPOSE_SIGN)
                         .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
-                        .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PSS).setUserConfirmationRequired(true)
+                        .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PSS).setUserConfirmationRequired(true).setIsStrongBoxBacked(true)
                         .build())
 
-        //data for ConfirmationPrompt
-        val myExtraData: ByteArray = byteArrayOf()
-        val myDialogData = ConfirmationPromptData("Pieris", "UoE", "£500")
-        val threadReceivingCallback = Executor { runnable -> runnable.run() }
-        val callback = MyConfirmationCallback()
+        val keypair=keyPairGenerator.generateKeyPair()
 
-        val dialog = ConfirmationPrompt.Builder(this)
-                .setPromptText("${myDialogData.sender}, send ${myDialogData.amount} to ${myDialogData.receiver}?")
-        .setExtraData(myExtraData)
-                .build()
-        dialog.presentPrompt(threadReceivingCallback, callback)
-*/
+
+     buttonTest.setOnClickListener() {
+         if (!fieldAmount.text.all { it.isDigit() }) {
+             Toast.makeText(this, "Amount must be a number!", Toast.LENGTH_LONG).show()
+         } else {
+             if (fieldName.text.isNullOrEmpty() || fieldAmount.text.isNullOrEmpty()) {
+                 Toast.makeText(this, "Please fill in both Name and Amount before pressing the Try button!", Toast.LENGTH_LONG).show()
+             } else {
+                 //data for ConfirmationPrompt
+                 val myExtraData: ByteArray = byteArrayOf()
+                 val myDialogData = ConfirmationPromptData("${fieldName.text}", "UoE", "£${fieldAmount.text}")
+                 val threadReceivingCallback = Executor { runnable -> runnable.run() }
+                 val callback = MyConfirmationCallback()
+
+                 val dialog = ConfirmationPrompt.Builder(this)
+                         .setPromptText("${myDialogData.sender}, send ${myDialogData.amount} to ${myDialogData.receiver}?")
+                         .setExtraData(myExtraData)
+                         .build()
+                 dialog.presentPrompt(threadReceivingCallback, callback)
+             }
+         }
+     }
     }
 }
